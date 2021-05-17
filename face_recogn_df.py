10463aa99ac937be0ef57757af23e497e28e0a4b
kolomieo <kolomieo.com>
2021-05-17 12:56:02 +0200

    Face regcogn with df

---
+++ face_recogn_df.py
@@ -0,0 +1,91 @@
+import face_recognition
+import cv2
+import numpy as np
+import os
+import glob
+import time
+from numpy.core.defchararray import index
+import pandas as pd
+import logging
+logging.basicConfig(level=logging.INFO)
+from datetime import datetime
+
+def extract_features():
+    faces_encodings = []
+    faces_names = []
+    cur_direc = os.getcwd()
+    path = os.path.join(cur_direc, 'faces/')
+    list_of_files = [f for f in glob.glob(path+'*.jpg')]
+    number_files = len(list_of_files)
+    names = list_of_files.copy()
+
+    logging.info('Extracting face features...')
+    for i in range(number_files):
+        globals()['image_{}'.format(i)] = face_recognition.load_image_file(list_of_files[i])
+        globals()['image_encoding_{}'.format(i)] = face_recognition.face_encodings(globals()['image_{}'.format(i)])[0]
+        faces_encodings.append(globals()['image_encoding_{}'.format(i)])
+        names[i] = names[i].replace(cur_direc, "")
+        faces_names.append(names[i])
+
+    logging.info('Appending ff\'s to df...')
+    df = pd.DataFrame(columns = [i for i in range(1,130)])
+    for encoding,name in zip(faces_encodings,names):
+        new = list(encoding)
+        new.insert(0,name)
+        df.loc[len(df)] = new
+    df.to_csv("face_features.csv", index=False)
+
+extract_features()
+df= pd.read_csv("face_features.csv")
+
+face_locations = []
+face_encodings = []
+face_names = []
+process_this_frame = True
+prev_frame_time = 0
+new_frame_time = 0 
+
+df_time = pd.DataFrame(columns = ['Name','Time'])
+video_capture = cv2.VideoCapture(0)
+while True:
+    ret, frame = video_capture.read()    
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)    
+    rgb_small_frame = small_frame[:, :, ::-1]    
+    if process_this_frame:
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)        
+        face_names = []
+        for face_encoding in face_encodings:
+                matches = face_recognition.compare_faces(df.iloc[:,1:129], face_encoding)
+                name = "Unknown"            
+                face_distances = face_recognition.face_distance(df.iloc[:,1:129], face_encoding)
+                best_match_index = np.argmin(face_distances)
+                if matches[best_match_index]:
+                    name = df.iloc[:,0][best_match_index]            
+                    face_names.append(name)
+                    entrance_time = datetime.now()
+                    df_time.loc[len(df)] = [name,entrance_time]
+
+    # FPS 
+    # new_frame_time = time.time()
+    # font = cv2.FONT_HERSHEY_SIMPLEX
+    # fps = 1/(new_frame_time-prev_frame_time)
+    # prev_frame_time = new_frame_time
+    # fps = int(fps)
+    # fps = str(fps)
+    # cv2.putText(frame, fps, (7, 70), font, 3, (100, 255, 0), 3, cv2.LINE_AA)
+
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4# Draw a rectangle around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)# Input text label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)# Display the resulting image
+
+    cv2.imshow('Video', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        print(df_time)
+        break
