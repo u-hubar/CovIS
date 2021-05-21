e871d4234eed734bde128860b40c7cf115afd0a9
u-hubar <hubar.uladzislau@gmail.com>
2021-05-21 17:35:13 +0200

    Added function for changing person enter permission and enter/exit time updating, implemented tests for CovisDB class

--- database/db.py
+++ database/db.py
@@ -1,4 +1,5 @@
 import logging
+import textwrap
 
 import psycopg2
 import psycopg2.extras
@@ -31,13 +32,26 @@ class CovisDB:
         insert_query = """
             INSERT INTO CovIS.Person (isStudent, isStaff, eligibleToEnter)
             VALUES (%s, %s, %s)
-            RETURNING idPerson"
+            RETURNING idPerson
         """
 
         cursor.execute(insert_query, person)
         person_id = cursor.fetchone()["idperson"]
 
         return person_id
+
+    @use_cursor
+    def check_enter_permission(self, person_id, cursor):
+        select_query = """
+            SELECT eligibleToEnter
+            FROM CovIS.Person
+            WHERE idPerson = %s
+        """
+
+        cursor.execute(select_query, (person_id,))
+        permission = cursor.fetchone()["eligibletoenter"]
+
+        return permission
 
     @use_cursor
     def update_person_status(self, person_id, elig, cursor):
@@ -50,12 +64,32 @@ class CovisDB:
         cursor.execute(update_query, (elig, person_id))
 
     @use_cursor
+    def update_enter_time(self, person_id, time, cursor):
+        update_query = """
+            UPDATE CovIS.Person
+            SET enterTime = %s
+            WHERE idPerson = %s
+        """
+
+        cursor.execute(update_query, (time, person_id))
+
+    @use_cursor
+    def update_exit_time(self, person_id, time, cursor):
+        update_query = """
+            UPDATE CovIS.Person
+            SET exitTime = %s
+            WHERE idPerson = %s
+        """
+
+        cursor.execute(update_query, (time, person_id))
+
+    @use_cursor
     def reset_time(self, cursor):
         update_query = """
             UPDATE CovIS.Person
             SET eligibleToEnter = FALSE,
-            SET enterTime = NULL,
-            SET exitTime = NULL
+            enterTime = NULL,
+            exitTime = NULL
         """
 
         cursor.execute(update_query)
